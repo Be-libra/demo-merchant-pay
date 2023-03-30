02dd7302f88ccc0a82d2ee95bb4ea2b06b375cfd
Be-libra <50490129+Be-libra@users.noreply.github.com>
2023-03-31 01:31:08 +0530

    feat: add  wallet connect

--- src/App.js
+++ src/App.js
@@ -1,89 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import {ethers} from "ethers"
-import { useState } from 'react';
-import getLinker from './deeplink';
-import mobileCheck from './mobileCheck';
-
-
-function App() {
-
-  const [isConnect, setIsConnect] = useState("")
-
-  // const onPressConnect = async () => {
-  //   setLoading(true);
-
-  //   try {
-  //     const yourWebUrl = "mysite.com"; // Replace with your domain
-  //     const deepLink = `https://metamask.app.link/dapp/${yourWebUrl}`;
-  //     const downloadMetamaskUrl = "https://metamask.io/download.html";
-
-  //     if (window?.ethereum?.isMetaMask) {
-  //       // Desktop browser
-  //       const accounts = await window.ethereum.request({
-  //         method: "eth_requestAccounts",
-  //       });
-  //     } else if (mobileCheck()) {
-  //       // Mobile browser
-  //       const linker = getLinker(downloadMetamaskUrl);
-  //       linker.openURL(deepLink);
-  //     } else {
-  //       window.open(downloadMetamaskUrl);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     setAddress("");
-  //   }
-  // };
-
-  const pay =async() =>{
-    try {
-      const currentURl = window.location.href
-      const yourWebUrl = currentURl.split("https://")[1]
-      const deepLink = `https://metamask.app.link/dapp/${yourWebUrl}`;
-      const downloadMetamaskUrl = "https://metamask.io/download.html";
-      if(mobileCheck() && !window?.ethereum){
-        const linker = getLinker(downloadMetamaskUrl);
-        linker.openURL(deepLink);
-      }
-      const provider = new ethers.BrowserProvider(window.ethereum)
-
-
-      // MetaMask requires requesting permission to connect users accounts
-      await provider.send("eth_requestAccounts", []);
-
-      // The MetaMask plugin also allows signing transactions to
-      // send ether and pay to change state within the blockchain.
-      // For this, you need the account signer...
-      const signer = provider.getSigner()
-
-      const tx = await (await signer).sendTransaction({
-        to: "0x77BC322f05f1728465428a9788612CFE029EeEC1",
-        value: ethers.parseEther(".01")
-      });
-
-      setIsConnect(tx.hash)
-      console.log(tx);
-      if(tx){
-        await tx.wait();
-        console.log(tx);
-        setIsConnect("new hash")
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+import { WagmiConfig, createClient, configureChains, mainnet, goerli } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import Payment from './Payment'
+ 
+const { chains, provider, webSocketProvider } = configureChains(
+  [goerli],
+  [publicProvider()],
+)
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={pay}>
-          pay
-        </button>
-        {isConnect}
-      </header>
-    </div>
-  );
+    <WagmiConfig client={client}>
+      <Payment />
+    </WagmiConfig>
+  )
 }
-
-export default App;
